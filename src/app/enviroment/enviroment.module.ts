fa5bc71979618564d5588d59c02ccc7c57fa4c5b
ComitterName <comitter@email.com>
2026-05-04 19:19:15 +0200

    submit file

--- src/app/enviroment/enviroment.module.ts
+++ src/app/enviroment/enviroment.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { ResizableModule } from 'angular-resizable-element';
+import { TableListComponent } from '../../../src/app/table-list/table-list.component';
+import { DashboardComponent } from '../../../src/app/dashboard/dashboard.component';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -16,6 +18,8 @@ import {MatSelectModule} from '@angular/material/select';
 @NgModule({
   declarations: [
     EnviromentComponent,
+    TableListComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule, 
